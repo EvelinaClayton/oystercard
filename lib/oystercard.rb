ad49bf2677aef3577c54a6ee37e6bd14e1808865
Guy Calloway <[guyfcalloway@gmail.com]>
2019-07-31 17:11:34 +0100

    step 12 adding journeys to log

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Oystercard
-  attr_reader :balance, :currently_in_use
-  
+  attr_reader :balance, :currently_in_use, :journey, :log
+
   DEFAULT_BALANCE = 0
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
@@ -10,30 +10,38 @@ class Oystercard
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
-    @currently_in_use = false
     @entry_station = nil
+    @exit_station = nil
+    @log = []
+    @journey = {"entry" => nil, "exit" => nil}
   end
 
   def top_up(amount)
     error_message = "Maximum balance of #{MAXIMUM_BALANCE} exceeded."
     raise error_message if amount + balance > MAXIMUM_BALANCE
-
     @balance += amount
   end
 
   def touch_in(station)
     fail "Insufficient balance to touch in" if @balance < MINIMUM_BALANCE
-    @currently_in_use = true
     @entry_station = station
+    @journey["entry"] = station
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MINIMUM_CHARGE)
-    @currently_in_use = false
+    @exit_station = station
+    @journey["exit"] = station
+    @log << @journey
+    @entry_station = nil
   end
+
+
+
+
 
   def in_journey?
-    @currently_in_use
+    !!@entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,9 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:entry_station){ double("station") }
+  let(:exit_station){ double("station") }
+
   it 'has a balance of 0' do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq 0
@@ -28,9 +31,9 @@ describe Oystercard do
     expect(subject).to respond_to(:deduct).with(1).argument
   end
 
-  it 'has a method touch_in' do
-    expect(subject).to respond_to(:touch_in)
-  end
+  # it 'has a method touch_in' do
+  #   expect(subject).to respond_to(:touch_in)
+  # end
 
   it 'has a method touch_out' do
     expect(subject).to respond_to(:touch_out)
@@ -42,26 +45,31 @@ describe Oystercard do
 
   it 'can touch in' do
     subject.top_up(1)
-    subject.touch_in 
-    expect(subject).to be_in_journey 
+    subject.touch_in(entry_station)
+    expect(subject).to be_in_journey
   end
 
   it 'can touch out' do
     subject.top_up(1)
-    subject.touch_in
-    expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE) 
-    subject.touch_out
+    subject.touch_in(entry_station)
+    expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
+    subject.touch_out(exit_station)
     expect(subject).not_to be_in_journey
   end
 
-  it 'will not touch in if below minimum balance' do 
-    expect{ subject.touch_in }.to raise_error "Insufficient balance to touch in"
+  it 'will not touch in if below minimum balance' do
+    expect{ subject.touch_in(entry_station) }.to raise_error "Insufficient balance to touch in"
   end
 
-  # In order to pay for my journey
-  # As a customer
-  # I need to know where I've travelled from
-  
 
+  it 'will save entry and exit stations in journey' do
+    subject.top_up(1)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.journey). to eq ({ "entry" => entry_station, "exit" => exit_station })
+  end
+
+  it 'has empty list called log by default' do
+    expect(subject.log).to be_empty
+  end
 end
-
